0d94ef4a869babe74472052f6a64a221a73008f8
Marcin Wiatr <marcinwiatr@hotmail.com>
2024-06-18 07:33:08 -0700

    Initial Commit

---
+++ TicTac/TicTacApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacApp.swift
+//  TicTac
+//
+//  Created by MarcinWiatr on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
